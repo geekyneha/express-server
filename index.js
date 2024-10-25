c8aaef21f443e84b83cccc37916f251c49bcd7b3
Neha <nehatualea@gmail.com>
2024-10-25 23:36:04 +0530

    added env variables

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
 const app= express();
-const PORT = 3000;
+const { config } = require("dotenv");
+const port = process.env.PORT;
+
 
 app.get('/',(req,res)=>{
     res.send('Hello World!')
 })
-app.listen(PORT,()=>{
+app.listen(port,()=>{
     console.log("serevr is running on ")
 })
